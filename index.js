5e86eb454b140bcb479f25d14d611b66fe16ae83
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-15 13:10:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories(){
   var repos = JSON.parse(this.responseText);
   console.log(this.responseText);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name +' - <a href="#" data-repo="'+r.name+ '</li>')
+    .map(r => '<li>' + r.name +' - <a href="#" data-repo="'+r.name+'" onclick=getCommits(this)">Get Commits</a></li>')
     .join('')}</ul>`;
   
   document.getElementById('repositories').innerHTML = repoList;
