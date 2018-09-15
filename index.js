b8e91c3bb61c65348c497bdc7dee72c6dedf916a
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-15 13:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ function getCommits(el){
   req.send();
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
+
 function getRepositories(){
   const req = new XMLHttpRequest();
   req.addEventListener('load',showRepositories);
